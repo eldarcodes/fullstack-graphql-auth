e31cf9af8618525bd6ac623b7365d3a043ccda2a
mirzabekov00 <prog.eldar@gmail.com>
2021-03-31 22:09:46 +0200

    Handling expired tokens

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,10 +5,13 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
+  ApolloLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { getAccessToken } from "./accessToken";
+import { getAccessToken, setAccessToken } from "./accessToken";
 import { App } from "./App";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
@@ -26,7 +29,41 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: "accessToken",
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
+        if (!token) return true;
+
+        try {
+          const { exp } = jwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch (error) {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch("http://localhost:4000/refresh_token", {
+          method: "POST",
+          credentials: "include",
+        });
+      },
+      handleFetch: (accessToken) => {
+        setAccessToken(accessToken);
+      },
+      handleError: (err) => {
+        console.warn("Your refresh token is invalid. Try to relogin");
+        console.error(err);
+      },
+    }),
+    authLink,
+    httpLink,
+  ]),
   cache: new InMemoryCache(),
 });
 
